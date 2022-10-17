21cd8392dbfcbdb342d19e8c05b946e76213108b
LukaLambrecht <Luka.Lambrecht@UGent.be>
2022-10-17 16:07:00 +0200

    small updates for ttW trigger measurement

---
+++ constants/luminosities.py
@@ -0,0 +1,11 @@
+# see for example https://twiki.cern.ch/twiki/bin/view/CMS/TopSystematics 
+# for the latest recommended numbers!
+
+lumidict = ({
+  '2016': 36330,
+  '2016PreVFP': 19520,
+  '2016PostVFP': 16810,
+  '2017': 41480,
+  '2018': 59830,
+  'run2': 137650
+})
