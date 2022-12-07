3e5018a4ec4ad657fef4a93fe49294d962e1e02f
LukaLambrecht <Luka.Lambrecht@UGent.be>
2022-12-07 13:22:31 +0100

    add particle level object classes

---
+++ objects/src/LeptonParticleLevel.cc
@@ -0,0 +1,72 @@
+#include "../interface/LeptonParticleLevel.h"
+
+//include c++ library classes
+#include <utility>
+#include <stdexcept>
+
+
+LeptonParticleLevel::LeptonParticleLevel( 
+    const TreeReader& treeReader, const unsigned index ) :
+    PhysicsObject( treeReader._pl_lPt[index], treeReader._pl_lEta[index], 
+		    treeReader._pl_lPhi[index], treeReader._pl_lE[index], 
+		    treeReader.is2016(), 
+		    treeReader.is2016PreVFP(),
+		    treeReader.is2016PostVFP(),
+		    treeReader.is2017(),
+		    treeReader.is2018() ),
+    _charge( treeReader._pl_lCharge[index] ),
+    _flavor( treeReader._pl_lFlavor[index] )
+    {}
+
+
+LeptonParticleLevel::~LeptonParticleLevel(){
+}
+
+
+void LeptonParticleLevel::copyNonPointerAttributes( const LeptonParticleLevel& rhs ){
+    _charge = rhs._charge;
+    _flavor = rhs._flavor;
+}
+
+
+LeptonParticleLevel& LeptonParticleLevel::operator=( const LeptonParticleLevel& rhs ){
+    //copy the PhysicsObject part of the Lepton
+    PhysicsObject::operator=(rhs); 
+    //copy non pointer members
+    copyNonPointerAttributes( rhs );
+    return *this;
+}
+
+
+LeptonParticleLevel& LeptonParticleLevel::operator=( LeptonParticleLevel&& rhs ) noexcept{
+    //move the PhysicsObject part of the lepton
+    PhysicsObject::operator=( std::move(rhs) );
+    //in case of self assignment the move assignment should do no work
+    if( this != &rhs ){
+        //copy non pointer members
+        copyNonPointerAttributes( rhs );
+    }
+    return *this;
+}
+
+
+bool sameFlavor( const LeptonParticleLevel& lhs, const LeptonParticleLevel& rhs ){
+    return ( 
+        ( lhs.isMuon() && rhs.isMuon() ) ||
+        ( lhs.isElectron() && rhs.isElectron() ) ||
+        ( lhs.isTau() && rhs.isTau() )
+    );
+}
+
+
+bool oppositeSign( const LeptonParticleLevel& lhs, const LeptonParticleLevel& rhs ){
+    return ( lhs.charge() != rhs.charge() );
+}
+
+
+std::ostream& LeptonParticleLevel::print( std::ostream& os) const{
+    PhysicsObject::print( os );
+    os << " / charge = " << ( _charge > 0 ? "+" : "-" );
+    os << " / flavor = " << _flavor;
+    return os;
+}
