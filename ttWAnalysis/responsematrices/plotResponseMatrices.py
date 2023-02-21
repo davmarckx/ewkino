dffbfc8faa7fe6cd9305beb6fa6efda7e7dae3a0
David Marckx <david.marckx@ugent.be>
2023-02-21 17:12:52 +0100

    still inbetween fix

--- ttWAnalysis/responsematrices/plotResponseMatrices.py
+++ ttWAnalysis/responsematrices/plotResponseMatrices.py
@@ -2,6 +2,7 @@
 # plot histograms obtained with fillResponseMatrix #
 ####################################################
 
+import ROOT
 import sys
 import os
 import argparse
@@ -14,6 +15,8 @@ sys.path.append(os.path.abspath('tools'))
 import responsematrixtools as rmt
 import responsematrixplotter as rmp
 
+#example: python3 plotResponseMatrices.py --inputfile output/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_Autumn18.root --outputdir ~/public_html/2018 --variables ../variables/variables_responsematrices.json --writebincontentauto --docustom --response
+
 if __name__=='__main__':
 
   # parse arguments
@@ -21,11 +24,12 @@ if __name__=='__main__':
   parser.add_argument('--inputfile', required=True, type=os.path.abspath)
   parser.add_argument('--outputdir', required=True)
   parser.add_argument('--variables', required=True, type=os.path.abspath)
-  parser.add_argument('--include_outerflow', default=False, action='store_true')
+  parser.add_argument('--include_outerflow', default=False, action='store_true') # in true definitions outerflow should be False
   parser.add_argument('--writebincontent', default=False, action='store_true')
   parser.add_argument('--writebincontentauto', default=False, action='store_true')
   parser.add_argument('--dogeneric', default=False, action='store_true')
   parser.add_argument('--docustom', default=False, action='store_true')
+  parser.add_argument('--response', default=False, action='store_true')
   args = parser.parse_args()
 
   # print arguments
@@ -50,6 +54,17 @@ if __name__=='__main__':
   # loop over variables
   for var in variables:
     varname = var.name
+    binnings = var.bins
+    if binnings == None:
+        binnings = []
+
+        binnr = var.nbins
+        xmin = var.xlow
+        xmax = var.xhigh
+        avrg = (xmax-xmin)/binnr
+        for i in range(binnr+1):
+            binnings.append(xmin + avrg*i)
+    
     axtitle = var.axtitle
     unit = var.unit
     print('now running on variable {}...'.format(varname))
@@ -76,14 +91,18 @@ if __name__=='__main__':
     # get normalized histograms, stability and purity
     colnormhist = rmt.normalize_columns(hist, include_outerflow=args.include_outerflow)
     rownormhist = rmt.normalize_rows(hist, include_outerflow=args.include_outerflow)
-    stability = rmt.get_stability(hist, include_outerflow=args.include_outerflow)
-    purity = rmt.get_purity(hist, include_outerflow=args.include_outerflow)
+    colnormhist_withunderflow = rmt.normalize_columns(hist, include_outerflow=True)
+
+    stability = rmt.get_stability(hist, include_outerflow=False)
+    purity = rmt.get_purity(hist, include_outerflow=False)
+    efficiency = rmt.get_efficiency(hist)
+
  
     # set common plot properties
     if( axtitle is not None and unit is not None ):
       axtitle += ' ({})'.format(unit)
     xaxtitle = 'Particle-level'
-    yaxtitle = 'Detectorl-level'
+    yaxtitle = 'Detector-level'
     xtitleoffset = 1.2
     ytitleoffset = 1.2
     ztitleoffset = 1.2
@@ -92,7 +111,10 @@ if __name__=='__main__':
     if args.writebincontent: drawoptions += 'texte'
     if args.writebincontentauto:
       if hist.GetNbinsX()<10: drawoptions += 'texte'
-    histtitle = 'Response matrix for '+axtitle[:1].lower()+axtitle[1:]
+    if args.response:
+        histtitle = 'Response matrix for '+axtitle[:1].lower()+axtitle[1:]
+    else:
+        histtitle = 'Resolution matrix for '+axtitle[:1].lower()+axtitle[1:]
     lumi = None
     lumitext = ''
     extracmstext = 'Preliminary'
@@ -101,7 +123,7 @@ if __name__=='__main__':
     # generic approach using simple 2D histogram plotter
     if args.dogeneric:
 
-      # make raw plot
+      # make raw plot, against common approach but useful for statistics
       outfile = os.path.join(args.outputdir, varname+'_absolute')
       h2dp.plot2dhistogram( hist, outfile, outfmts=['.png'],
             histtitle=histtitle,
@@ -112,7 +134,7 @@ if __name__=='__main__':
     	    docmstext=True, lumitext=lumitext, extracmstext=extracmstext,
             extrainfos=extrainfos, infosize=None, infoleft=None, infotop=None )
 
-      # make column-normalized plot
+      # make column-normalized plot, against common approach
       outfile = os.path.join(args.outputdir, varname+'_colnorm')
       h2dp.plot2dhistogram( colnormhist, outfile, outfmts=['.png'],
             histtitle=histtitle,
@@ -136,10 +158,23 @@ if __name__=='__main__':
 
     # custom approach using dedicated plotting function
     if args.docustom:
-      outfile = os.path.join(args.outputdir, varname+'_custom')
       xaxtitle = 'Particle level '+axtitle[:1].lower()+axtitle[1:]
       yaxtitle = 'Detector level '+axtitle[:1].lower()+axtitle[1:]
-      rmp.plotresponsematrix( hist,efficiency, stability, purity, outfile, outfmts=['.png'],
+
+      if args.response:
+          outfile = os.path.join(args.outputdir, 'responsetest' + varname + '_custom')
+          # we also need to make the underflow bin part of the new histo but at the top. ugly fix is to make a new one and fill by hand.
+          hist = rmt.AddUnderflowBins(colnormhist_withunderflow, binnings)
+
+          rmp.plotresponsematrix(hist, efficiency, stability, purity, outfile, outfmts=['.png'],
+            xtitle=xaxtitle, ytitle=yaxtitle, ztitle='Number of events',
+            drawoptions=drawoptions,
+            lumitext=lumitext, extracmstext=extracmstext,
+            extrainfos=extrainfos, infosize=None, infoleft=None, infotop=None )
+
+      else:
+          outfile = os.path.join(args.outputdir, 'resolutiontest' + varname+'_custom')
+          rmp.plotresponsematrix( colnormhist, efficiency, stability, purity, outfile, outfmts=['.png'],
             xtitle=xaxtitle, ytitle=yaxtitle, ztitle='Number of events',
             drawoptions=drawoptions,
             lumitext=lumitext, extracmstext=extracmstext,
