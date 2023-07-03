67eecd6696d44a14ac8313e3b29e5111a9e9daad
David Marckx <david.marckx@ugent.be>
2023-07-03 14:18:07 +0200

    everything ready for the merge. added samplelists with EW corrections (currently xsec of all EW NLO corrections stated in NNLO QCD ttW paper). also added fix for btagging systematics nominal histograms for data.also contains the fix of selecting the right processes for PL split plots from a merged file. removed occurences of test for NP oviedo CR test

--- ttWAnalysis/analysis/merge_submit.py
+++ ttWAnalysis/analysis/merge_submit.py
@@ -11,15 +11,15 @@ from jobSettings import CMSSW_VERSION
 topdir = sys.argv[1]
 
 regions = []
-for r in ['signalregion_dilepton_oviedo']: regions.append(r)
-for r in ['npcontrolregion_dilepton_oviedo']: regions.append(r)
-#for r in ['signalregion_dilepton_inclusive']: regions.append(r)
+#for r in ['signalregion_dilepton_oviedo']: regions.append(r)
+#for r in ['npcontrolregion_dilepton_oviedo']: regions.append(r)
+for r in ['signalregion_dilepton_inclusive']: regions.append(r)
 #for r in ['ee','em','me','mm']: regions.append('signalregion_dilepton_{}'.format(r))
 #for r in ['plus','minus']: regions.append('signalregion_dilepton_{}'.format(r))
 #for r in ['signalregion_trilepton']: regions.append(r)
 #for r in ['wzcontrolregion','zzcontrolregion','zgcontrolregion']: regions.append(r)
 #for r in ['trileptoncontrolregion','fourleptoncontrolregion']: regions.append(r)
-#for r in ['npcontrolregion_dilepton_inclusive']: regions.append(r)
+for r in ['npcontrolregion_dilepton_inclusive']: regions.append(r)
 #for r in ['ee','em','me','mm']: regions.append('npcontrolregion_dilepton_{}'.format(r))
 #for r in ['cfcontrolregion']: regions.append(r)
 
