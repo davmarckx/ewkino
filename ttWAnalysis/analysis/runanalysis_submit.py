bca55471df48d4bc83bf0ba1ee6730ae736fa3bb
LukaLambrecht <Luka.Lambrecht@UGent.be>
2023-09-15 16:32:37 +0200

    current status of submission (backgrounds)

--- ttWAnalysis/analysis/runanalysis_submit.py
+++ ttWAnalysis/analysis/runanalysis_submit.py
@@ -13,19 +13,19 @@ exe = 'runanalysis'
 
 regions = []
 for r in ['signalregion_dilepton_inclusive']: regions.append(r)
-#for r in ['ee','em','me','mm']: regions.append('signalregion_dilepton_{}'.format(r))
-#for r in ['plus','minus']: regions.append('signalregion_dilepton_{}'.format(r))
-#for r in ['signalregion_trilepton']: regions.append(r)
-#for r in ['wzcontrolregion','zzcontrolregion','zgcontrolregion']: regions.append(r)
-#for r in ['trileptoncontrolregion','fourleptoncontrolregion']: regions.append(r)
-#for r in ['npcontrolregion_dilepton_inclusive']: regions.append(r)
-#for r in ['ee','em','me','mm']: regions.append('npcontrolregion_dilepton_{}'.format(r))
-#for r in ['nplownjetscontrolregion_dilepton_inclusive']: regions.append(r)
-#for r in ['cfcontrolregion']: regions.append(r)
-#for r in ['cfjetscontrolregion']: regions.append(r)
+for r in ['ee','em','me','mm']: regions.append('signalregion_dilepton_{}'.format(r))
+for r in ['plus','minus']: regions.append('signalregion_dilepton_{}'.format(r))
+for r in ['signalregion_trilepton']: regions.append(r)
+for r in ['wzcontrolregion','zzcontrolregion','zgcontrolregion']: regions.append(r)
+for r in ['trileptoncontrolregion','fourleptoncontrolregion']: regions.append(r)
+for r in ['npcontrolregion_dilepton_inclusive']: regions.append(r)
+for r in ['ee','em','me','mm']: regions.append('npcontrolregion_dilepton_{}'.format(r))
+for r in ['nplownjetscontrolregion_dilepton_inclusive']: regions.append(r)
+for r in ['cfcontrolregion']: regions.append(r)
+for r in ['cfjetscontrolregion']: regions.append(r)
 
-#years = ['2016PreVFP','2016PostVFP','2017','2018']
-years = ['2016PreVFP']
+years = ['2016PreVFP','2016PostVFP','2017','2018']
+#years = ['2016PreVFP']
 
 dtypes = ['sim','data']
 #dtypes = ['sim']
@@ -44,21 +44,22 @@ selection_types.append('irreducible')
 frdir = '../fakerates/fakeRateMaps_v20220912_tttt'
 cfdir = '../chargefliprates/chargeFlipMaps_v20221109'
 
-samplelistdir = '../samplelists/fourtops' # main sample lists
-samplelistbase = 'samples_tttt_{}_{}.txt' # main sample lists
+samplelistdir = '../samplelists/backgrounds' # background sample lists
+samplelistbase = 'samples_tttt_{}_{}.txt' # background sample lists
 #samplelistdir = 'samplelists' # sample lists for testing
 #samplelistbase = 'samplelist_test_{}_WZ.txt' # sample lists for testing
 #samplelistdir = '../samplelists/particlelevel' # sample lists for TTW signal samples
 #samplelistbase = 'samplelist_{}_TTW_particlelevel.txt' # sample lists for TTW signal samples
 
 #variables = '../variables/variables_main.json' # single variables
-variables = '../variables/variables_eventbdt.json' # bdt variable
+variables = '../variables/variables_main_reduced.json' # single variables (slightly reduced set)
+#variables = '../variables/variables_eventbdt.json' # bdt variable
 #variables = '../variables/variables_crfit.json' # reduced set of variables for CRs in fit
 #variables = '../variables/variables_particlelevel_double.json' # double variables
 
 #bdtfile = None
-#bdtfile = '../bdtweights/v20230601/XGBrobustnessv3_all.root'
-bdtfile = '../../ML/models/XGBrobustnessv3_all.root'
+bdtfile = '../bdtweights/v20230601/XGBrobustnessv3_all.root'
+#bdtfile = '../../ML/models/XGBrobustnessv3_all.root'
 bdtcut = None
 
 splitprocess = None # do not split any process at particle level
@@ -66,7 +67,7 @@ splitprocess = None # do not split any process at particle level
 splitvariables = None
 #splitvariables = '../variables/variables_particlelevel_single.json'
 
-outputdir = 'output_NPbugfix_afterv2'
+outputdir = 'output_20230915'
 
 nevents = 1e6
 runlocal = False
