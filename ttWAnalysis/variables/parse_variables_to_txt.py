f9d59648a2a33bd1e3b397aac9bc5e087a178f40
LukaLambrecht <Luka.Lambrecht@UGent.be>
2024-04-16 10:54:49 +0200

    update variable binning

--- ttWAnalysis/variables/parse_variables_to_txt.py
+++ ttWAnalysis/variables/parse_variables_to_txt.py
@@ -9,11 +9,21 @@ import variabletools as vt
 
 if __name__=='__main__':
 
+  # read input file(s)
   inputfile = sys.argv[1]
-  outputfile = inputfile.replace('.json','.txt')
-  
-  if os.path.exists(outputfile):
-    raise Exception('ERROR: output file already exists.')
+  if inputfile=='all':
+    inputfiles = [f for f in os.listdir('.') if f.endswith('.json')]
+  else: inputfiles = [inputfile]
 
-  varlist = vt.read_variables( inputfile )
-  vt.write_variables_txt( varlist, outputfile ) 
+  # loop over input files
+  for inputfile in inputfiles:
+    print('Parsing file {} to txt file...'.format(inputfile))
+    
+    # define and check output file
+    outputfile = inputfile.replace('.json','.txt')
+    if os.path.exists(outputfile):
+      raise Exception('ERROR: output file already exists.')
+
+    # read variables from json and write to txt
+    varlist = vt.read_variables( inputfile )
+    vt.write_variables_txt( varlist, outputfile ) 
